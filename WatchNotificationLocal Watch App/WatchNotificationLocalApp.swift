6f7431494b041d71bc13a752bbbb50247891a039
Kevin Marin <kevinmarinsaavedra@gmail.com>
2023-07-26 15:01:55 +0200

    Initial Commit

---
+++ WatchNotificationLocal Watch App/WatchNotificationLocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchNotificationLocalApp.swift
+//  WatchNotificationLocal Watch App
+//
+//  Created by Kevin Marin on 26/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchNotificationLocal_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
